8e2f510843899086aaca6d8fc26e456c464cb52e
Samiq-56 <bsdsf24m056@pucit.edu.pk>
2025-01-27 23:45:18 +0500

    Add files via upload

---
+++ JAVA.js
@@ -0,0 +1,24 @@
+function first(){
+    alert("Welcome To my Website");
+ }
+
+function second(){
+    document.getElementById("card-img").src = "messi2.jpg";
+ }
+ function third(){
+    document.getElementById("card-img1").src = "ronaldo2.jpg";
+ }
+ function forth(){
+    document.getElementById("card-img2").src = "naymer.jpg";
+ }
+function secondLeave(){
+    document.getElementById("card-img").src = "messi.jpg";
+ }
+ function thirdLeave(){
+    document.getElementById("card-img1").src = "ronaldo.jpg";
+ }
+ function forthLeave(){
+    document.getElementById("card-img2").src = "naymer4.jpg";
+ }
+
+
